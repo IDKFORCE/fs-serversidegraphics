bf1c2d819fcd6ececec743a3a7aa39da2ea2a4a4
IDKFORCE <71199348+IDKFORCE@users.noreply.github.com>
2023-05-23 14:40:44 -0700

    first commit

---
+++ client.lua
@@ -0,0 +1,38 @@
+function stringsplit(inputstr, sep)
+	if sep == nil then
+		sep = "%s"
+	end
+
+	local t={} ; i=1
+
+	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+		t[i] = str
+		i = i + 1
+	end
+
+	return t
+end
+
+local function starts_with(str, start)
+   return str:sub(1, #start) == start
+end
+
+CreateThread(function()
+	local settingsFile = LoadResourceFile(GetCurrentResourceName(), "visualsettings.dat")
+
+	local lines = stringsplit(settingsFile, "\n")
+
+	for k,v in ipairs(lines) do
+		if not starts_with(v, '#') and not starts_with(v, '//') and (v ~= "" or v ~= " ") and #v > 1 then
+			v = v:gsub("%s+", " ")
+
+			local setting = stringsplit(v, " ")
+
+			if setting[1] ~= nil and setting[2] ~= nil and tonumber(setting[2]) ~= nil then
+				if setting[1] ~= 'weather.CycleDuration' then	
+					citizen.InvokeNative(GetHashKey('SET_VISUAL_SETTING_FLOAT') & 0xFFFFFFFF, setting[1], tonumber(setting[2])+.0)
+				end
+			end
+		end
+	end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,39 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '⸸♱♥IDK_FORCE♥♱⸸#7611'
+description 'ServerSide Graphics'
+
+files {
+	'stream/fxweather.ytd',
+	'stream/graphics.ytd',
+	'stream/graphics_pc.ytd',
+	'stream/skydome.ytd',
+	'data/hbaosettings.xml',
+    'data/visualsettings.dat',
+    'data/cloudkeyframes.xml',
+    'data/clouds.xml',
+    'data/cpqsmix_ssaosettings.xml',
+    'data/lensflare_f.xml',
+    'data/lensflare_t.xml',
+    'data/lensflare_m.xml',
+    'data/lensflare_t.xml',
+    'data/watertune.xml',
+    'data/levels/gta5/desert_emitter_ground.xml',
+    'data/levels/gta5/desert_emitter_ground_heavy.xml',
+    'data/levels/gta5/desert_render_ground.xml',
+    'data/levels/gta5/desert_render_ground_heavy.xml',
+    'data/levels/gta5/water.xml',
+    'data/timecycle/w_clear.xml',
+    'data/timecycle/w_clearing.xml',
+    'data/timecycle/w_extrasunny.xml',
+    'data/timecycle/w_foggy.xml',
+    'data/timecycle/w_overcast.xml',
+    'data/timecycle/w_rain.xml',
+    'data/timecycle/w_thunder.xml',
+}
+
+client_script 'client.lua'
+
+
+this_is_a_map 'yes'
